2444a411568ea103662082259dc3c3388d61f8c7
Tirta <tirta@gmail.com>
2017-12-04 15:35:26 -0200

    port heroku

--- crud.js
+++ crud.js
@@ -35,7 +35,7 @@ app.get('/', function (req, res) {
     res.send('ITS WORKING');
 });
 
-app.listen(5000, function () {
+app.listen(process.env.PORT || 5000, function () {
     console.log('Servidor rodando na porta 3001!')
 });
 
